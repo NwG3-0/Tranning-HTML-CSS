628fbf53662c0405cd7f988b8286caa78f4b8649
unknown <remon.hans@gmail.com>
2022-04-23 17:08:17 +0700

    ngay 9 gi do

---
+++ js/mainjs.js
@@ -0,0 +1,7 @@
+function myfunction3() {
+    document.getElementById("change3").src = "https://kubet88.kim/wp-content/uploads/2021/08/8-3.jpg";
+}
+
+function myfunction4() {
+    document.getElementById("change3").src = "https://pbs.twimg.com/media/E4GIZZ5VUAQmSwX?format=jpg&name=medium";
+}
